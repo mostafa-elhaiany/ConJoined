9e1096f6c9b31cbf7e50b078fa743e834e90e88d
mostafa-elhaiany <mostafa_elhaiany@outlook.com>
2021-06-12 19:32:51 +0200

    Feat: tutorial level setup + event system

---
+++ Assets/Scripts/UI/Statics/Event.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Event 
+{
+    public GameObject[] activate;
+    public GameObject[] deactivate;
+
+    public bool timerBased;
+    public float timeToFinish;
+
+
+
+}
