e20db7b37b1f8791172cbbf45cc408f142078f93
mostafa-elhaiany <mostafa_elhaiany@outlook.com>
2021-06-12 19:08:27 +0200

    Fix: show joining text

--- Assets/External assits/IndieMarc/TopDownDemo/Scripts/JoiningMechanics.cs
+++ Assets/External assits/IndieMarc/TopDownDemo/Scripts/JoiningMechanics.cs
@@ -1,38 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JoiningMechanics : MonoBehaviour
 {
     public bool inProximity;
-        public GameObject lightCharachter;
+    public GameObject lightCharachter;
 
-        bool isMoving;
+    bool isMoving;
 
-        public bool joined; 
-    // Start is called before the first frame update
+    public bool joined;
+
+    GameObject text;
+
     void Start()
     {
-        
+        text = GameObject.FindGameObjectWithTag("ingameUI");
     }
-
-    // Update is called once per frame
     void Update()
     {
         if(Vector2.Distance(this.gameObject.transform.position, lightCharachter.transform.position)<2f){
                 inProximity=true;
-            }
-            else{
+        }
+        else{
+            inProximity=false;
+        }
+        if(inProximity && Input.GetKeyDown(KeyCode.Space)){
+            joined = !joined;
+        }
 
-                inProximity=false;
-            }
-
-
-            if(inProximity && Input.GetKeyDown(KeyCode.Space)){
-                    joined = !joined;
-            }
-
-            
-        
+        text.SetActive(inProximity && !joined);
     }
 }
