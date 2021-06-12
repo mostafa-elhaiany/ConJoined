67a34ebb6d7d45a13dde40eac9e71ccea3d48c1a
mostafa-elhaiany <mostafa_elhaiany@outlook.com>
2021-06-12 16:28:34 +0200

    Feat: Splash Screen

---
+++ Assets/Scripts/UI/SplashScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class SplashScript : MonoBehaviour
+{
+    [SerializeField]
+    float waitTime=5f;
+
+    [SerializeField]
+    string scene="Main Menu";
+
+    float timeLeft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeft = waitTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft <= 0)
+            SceneHandler.changeScene(scene);
+        
+    }
+}
