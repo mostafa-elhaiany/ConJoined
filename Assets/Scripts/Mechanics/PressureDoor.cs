c148301e01c8c536090788ee876a4ed752c3a015
mostafa-elhaiany <mostafa_elhaiany@outlook.com>
2021-06-12 17:39:37 +0200

    Feat: pressurePlate
    scripts and a tester level

---
+++ Assets/Scripts/Mechanics/PressureDoor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressureDoor : MonoBehaviour
+{
+    [SerializeField]
+    bool doorOpened = false;
+
+    [SerializeField]
+    Transform closedPosition;
+    [SerializeField]
+    Transform openedPosition;
+
+    Vector2 position;
+
+    [SerializeField]
+    float moveValue = 0.1f;
+
+    public void toggleState()
+    {
+        doorOpened = !doorOpened;
+    }
+
+    void Update()
+    {
+        if (doorOpened)
+            position = openedPosition.position;
+        else
+            position = closedPosition.position;
+
+        Vector2 curPosition = new Vector2(this.transform.position.x, this.transform.position.y);
+        
+        this.transform.position = Vector2.Lerp(curPosition, position, moveValue * Time.deltaTime);
+
+    }
+}
