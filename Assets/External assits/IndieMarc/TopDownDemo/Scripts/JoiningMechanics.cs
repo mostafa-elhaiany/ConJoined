fc564ade193c53b0c2d58de15c6d723cd9ef7edc
zainab <zainabsultan1111@gmail.com>
2021-06-12 18:51:43 +0200

    player joining mechaics

---
+++ Assets/External assits/IndieMarc/TopDownDemo/Scripts/JoiningMechanics.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoiningMechanics : MonoBehaviour
+{
+    public bool inProximity;
+        public GameObject lightCharachter;
+
+        bool isMoving;
+
+        public bool joined; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector2.Distance(this.gameObject.transform.position, lightCharachter.transform.position)<2f){
+                inProximity=true;
+            }
+            else{
+
+                inProximity=false;
+            }
+
+
+            if(inProximity && Input.GetKeyDown(KeyCode.Space)){
+                    joined = !joined;
+            }
+
+            
+        
+    }
+}
