e5632c22ad1c47cc5824be7bcdcbede99ad40229
ahmed elzamarany <ahmed_elzamarany@hotmail.com>
2021-06-13 12:34:51 +0200

    fix errors

--- Assets/Scripts/Mechanics/PressurePlate.cs
+++ Assets/Scripts/Mechanics/PressurePlate.cs
@@ -1,20 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-
 public class PressurePlate : MonoBehaviour
 {
     [SerializeField]
-    PressureDoor door;
+    List<PressureDoor> PressureDoor_list = new List<PressureDoor>();
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        door.toggleState();
+        // door.toggleState();
+        foreach (PressureDoor door in PressureDoor_list)
+            {
+               
+                door.toggleState();
+            }
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
+        foreach (PressureDoor door in PressureDoor_list)
+            {
+               
         door.toggleState();
+            }
     }
 }
