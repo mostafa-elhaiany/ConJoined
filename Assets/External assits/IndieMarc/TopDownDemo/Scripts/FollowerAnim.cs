3cac1a485025a8af5e590675ed5bb350d79cf99c
MalakRassem <47498774+MalakRassem@users.noreply.github.com>
2021-06-13 19:12:17 +0200

    Light Character

---
+++ Assets/External assits/IndieMarc/TopDownDemo/Scripts/FollowerAnim.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IndieMarc.TopDown
+{
+    
+    [RequireComponent(typeof(Animator))]
+    public class FollowerAnim : MonoBehaviour
+    {
+        private GameObject mc;
+        private CharacterHoldItem character_item;
+        private PlayerCharacter character;
+        private Animator animator;
+
+        void Awake()
+        {
+            mc = GameObject.FindGameObjectsWithTag("Player")[0];
+            character = mc.GetComponent<PlayerCharacter>();
+            character_item = GetComponent<CharacterHoldItem>();
+            animator = GetComponent<Animator>();
+        }
+
+        void Update()
+        {
+            //Anims
+            animator.SetFloat("Speed", character.GetMove().magnitude);
+            animator.SetInteger("Side", character.GetSideAnim());
+            if (character_item != null)
+                animator.SetBool("Hold", character_item.GetHeldItem() != null);
+        }
+
+    }
+
+}
