1db70295d01c20d03689ae87182e2e6957bbb7dc
zainab <zainabsultan1111@gmail.com>
2021-06-13 14:39:17 +0200

    Light character edits

---
+++ Assets/Scripts/Mechanics/LightCharacterBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class LightCharacterBehaviour : MonoBehaviour
+{
+   
+    GameObject player; //the enemy's target
+       private Rigidbody2D rb;
+       float speed = 2f;
+    void Start()
+    {
+    
+        player =  GameObject.FindGameObjectsWithTag("Player")[0];
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if(player.GetComponent<JoiningMechanics>().joined  &&  Vector2.Distance(transform.position, player.transform.position)>1.0f ){
+        
+         float step = speed * Time.deltaTime;
+
+        // move sprite towards the target location
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, step);
+        }
+    }
+}
